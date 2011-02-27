c32789e1012367831a86d64523c99c5b5fff26d7
Benny Tsai <benny.tsai@gmail.com>
2011-02-27 16:02:16 -0700

    refactoring

--- sleeping_barber.clj
+++ sleeping_barber.clj
@@ -24,61 +24,56 @@
 
 (def waiting-room
   (actor
-   {:waiting nil :barber-asleep? true}
-   (fn [{:keys [waiting barber-asleep?] :as state}
-	[msg-type & args]
+   {:queue nil :asleep? true}
+   (fn [{:keys [queue asleep?] :as state}
+	[msg-type & _]
 	sender]
-     (let [self *agent*]
+     (let [self *agent*
+	   full? (= (count queue) *capacity*)]
        (case msg-type
-	     :enter (let [customer (first args)]
-		      (if (= (count waiting) *capacity*)
-			(do
-			  (send-msg sender :full)
-			  state)
-			(if barber-asleep?
-			  (do
-			    (send-msg self :next)
-			    (assoc state
-			      :waiting (cons customer waiting)
-			      :barber-asleep? false))
-			  (do
-			    (send-msg sender :wait)
-			    (assoc state
-			      :waiting (cons customer waiting))))))
-	     :next (if (> (count waiting) 0)
+	     :enter (cond
+		     full? (do
+			     (send-msg sender :full)
+			     state)
+		     asleep? (do
+			       (send-msg self :next)
+			       (assoc state
+				 :queue (cons sender queue)
+				 :asleep? false))
+		     :else (do
+			     (send-msg sender :wait)
+			     (assoc state :queue (cons sender queue))))
+	     :next (if (seq queue)
 		     (do
-		       (send-msg barber :enter (last waiting))
-		       (assoc state
-			 :waiting (butlast waiting)))
+		       (send-msg barber :enter (last queue))
+		       (assoc state :queue (butlast queue)))
 		     (do
 		       (send-msg barber :wait)
-		       (assoc state
-			 :barber-asleep? true)))
+		       (assoc state :asleep? true)))
 	     state)))))
 
 (defn customer [id]
   (actor
    id
-   (fn [id [msg-type & args] sender]
-     (let [self *agent*]
-       (case msg-type
-	     :enter (do
-		      (actor-println "Customer" id "is entering.")
-		      (send-msg waiting-room :enter self)
-		      id)
-	     :full (do
-		     (actor-println "Customer" id "is leaving.")
-		     id)
-	     :wait (do
-		     (actor-println "Customer" id "is waiting.")
-		     id)
-	     :start (do
-		      (actor-println "Customer" id "is being served.")
-		      id)
-	     :done (do
-		     (actor-println "Customer" id "has been served.")
-		     id)
-	     id)))))
+   (fn [id [msg-type & _] _]
+     (case msg-type
+	   :enter (do
+		    (actor-println "Customer" id "is entering.")
+		    (send-msg waiting-room :enter)
+		    id)
+	   :full (do
+		   (actor-println "Customer" id "is leaving.")
+		   id)
+	   :wait (do
+		   (actor-println "Customer" id "is waiting.")
+		   id)
+	   :start (do
+		    (actor-println "Customer" id "is being served.")
+		    id)
+	   :done (do
+		   (actor-println "Customer" id "has been served.")
+		   id)
+	   id))))
 
 (defn sleeping-barber []
   (send-msg barber :wait)
