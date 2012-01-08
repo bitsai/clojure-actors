f6048c316e60c9b56028fd6fd619f4823c46dcf0
Benny Tsai <benny.tsai@gmail.com>
2012-01-08 15:15:53 -0700

    update core.match dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clojure-actors "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Actors implemented on top of Clojure agents."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.match "0.2.0-alpha8"]])
+                 [org.clojure/core.match "0.2.0-alpha9"]])
